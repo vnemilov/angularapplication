cc4e8b159ade834140d487b4f9466658e2d983e7
vnemilov <vnemilov@yahoo.com>
2015-12-02 17:49:24 +0200

    messy branch with some stuff in it... NOT GOOD TO BE USED

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
 /**
  * Created by vemilov on 2.12.2015 г..
  */
-var routerApp = angular.module('routerApp', ['ui.router']);
+var routerApp = angular.module('routerApp', ['ngRoute','ui.router']);
 
-routerApp.config(function($stateProvider, $urlRouterProvider) {
+routerApp.config(function ($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/home');
 
     $stateProvider
 
-        // HOME STATES AND NESTED VIEWS ========================================
+    // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
             url: '/home',
             templateUrl: '../views/partial-home.html'
@@ -20,21 +19,27 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
             url: '/about',
             views: {
                 '': {templateUrl: '../views/partial-about.html'},
-                'columnOne@about': { template: 'Look I am a column!' },
+                'columnOne@about': {
+                    templateUrl: '../views/table-data2.html',
+                    controller: 'scotchController'
+                },
                 'columnTwo@about': {
                     templateUrl: '../views/table-data.html',
-                    controller: 'scotchController'
+                    controller: 'laptopController'
                 }
             }
+        })
 
+        //INFO ABOUT LAPTOPS
+        .state('laptops', {
+            url: '#/laptops/:laptopId}',
+            templateUrl: '../views/phone-detail.html',
+            controller: 'laptopInfoController'
         });
-
 });
 
-routerApp.controller('scotchController', function($scope) {
-
+routerApp.controller('scotchController', function ($scope) {
     $scope.message = 'test';
-
     $scope.scotches = [
         {
             name: 'Macallan 12',
@@ -49,5 +54,17 @@ routerApp.controller('scotchController', function($scope) {
             price: 20000
         }
     ];
-
 });
+
+routerApp.controller('laptopController', function ($scope, $http) {
+    $http.get('../laptops.json').success(function (data) {
+        $scope.laptops = data;
+    });
+});
+
+routerApp.controller('laptopInfoController', ['$scope', '$routeParams', '$http',
+    function ($scope, $routeParams, $http) {
+    $http.get('../laptops/'+ $routeParams.laptopId + '.json').success(function (data) {
+        $scope.laptop = data;
+    });
+}]);
