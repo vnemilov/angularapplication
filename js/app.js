2f04e0a44d5b221f815917891dd939cf44e0a274
vnemilov <vnemilov@yahoo.com>
2015-12-02 14:16:16 +0200

    Rearanged the files into folders, removed some of the code which won't be used for the purpose of the application.

--- js/app.js
+++ js/app.js
@@ -12,32 +12,17 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
         // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
             url: '/home',
-            templateUrl: 'partial-home.html'
-        })
-
-        // nested list with custom controller
-        .state('home.list', {
-            url: '/list',
-            templateUrl: 'partial-home-list.html',
-            controller: function($scope) {
-                $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
-            }
-        })
-
-        // nested list with just some random string data
-        .state('home.paragraph', {
-            url: '/paragraph',
-            template: 'I could sure use a drink right now.'
+            templateUrl: '../views/partial-home.html'
         })
 
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('about', {
             url: '/about',
             views: {
-                '': { templateUrl: 'partial-about.html' },
+                '': {templateUrl: '../views/partial-about.html'},
                 'columnOne@about': { template: 'Look I am a column!' },
                 'columnTwo@about': {
-                    templateUrl: 'table-data.html',
+                    templateUrl: '../views/table-data.html',
                     controller: 'scotchController'
                 }
             }
